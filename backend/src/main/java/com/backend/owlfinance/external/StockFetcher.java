c2856fcd1b06954fbb35a67810b38a68ac957c0a
bruce <wanghaotian1014@gmail.com>
2024-10-27 14:10:56 -0500

    cleaned

--- backend/src/main/java/com/backend/owlfinance/external/StockFetcher.java
+++ backend/src/main/java/com/backend/owlfinance/external/StockFetcher.java
@@ -1,4 +1,4 @@
-package com.backend.owlfinance.external;
+package org.example;
 
 import java.io.BufferedReader;
 import java.io.FileWriter;
@@ -17,11 +17,11 @@ public class StockFetcher {
     private static final String Alpha_URL = "https://www.alphavantage.co/query";
 
 
-    public static void getMinuteByMinuteHistoricalPrices(String stockCode, String targetDate) throws Exception {
+    public static void getMinuteByMinuteHistoricalPrices(String stockCode, String targetMonth) throws Exception {
         String function = "TIME_SERIES_INTRADAY";
         String interval = "1min"; // Fetching minute-by-minute data
         String urlString = Alpha_URL + "?function=" + function + "&symbol=" + stockCode
-                + "&interval=" + interval + "&outputsize=full" + "&apikey=" + Alpha_API;
+                + "&interval=" + interval + "&month=" + targetMonth + "&outputsize=full" + "&apikey=" + Alpha_API;
 
         HttpURLConnection connection = (HttpURLConnection) new URL(urlString).openConnection();
         connection.setRequestMethod("GET");
@@ -37,26 +37,18 @@ public class StockFetcher {
 
         JSONObject jsonResponse = new JSONObject(response.toString());
 
-        saveJSONToFile(jsonResponse, stockCode + "_historical_data_" + targetDate + ".json");
 
-        JSONObject timeSeries = jsonResponse.getJSONObject("Time Series (" + interval + ")");
 
-        List<String> timeKeys = new ArrayList<>(timeSeries.keySet());
-        Collections.sort(timeKeys);
+//        JSONObject timeSeries = jsonResponse.getJSONObject("Time Series (" + interval + ")");
+//
+//        List<String> timeKeys = new ArrayList<>(timeSeries.keySet());
+//        Collections.sort(timeKeys);
 
-        System.out.println("Minute-by-Minute Historical Prices for " + stockCode + " on " + targetDate + ":");
-        boolean dataFound = false;
-        for (String time : timeKeys) {
-            if (time.startsWith(targetDate)) {
-                dataFound = true;
-                JSONObject minuteData = timeSeries.getJSONObject(time);
-                System.out.println("Time: " + time + ", Close Price: " + minuteData.getDouble("4. close"));
-            }
-        }
+        saveJSONToFile(jsonResponse, stockCode + "_historical_data_" + targetMonth + ".json");
 
-        if (!dataFound) {
-            System.out.println("No data found for the specified date: " + targetDate);
-        }
+
+
+
     }
 
     public static void saveJSONToFile(JSONObject jsonObject, String fileName) {
@@ -71,7 +63,7 @@ public class StockFetcher {
     public static void main(String[] args) {
         try {
             String stockCode = "AAPL";
-            String targetDate = "2024-09-27";
+            String targetDate = "2024-08";
 
             getMinuteByMinuteHistoricalPrices(stockCode, targetDate);
 
