fe4864f452561fffbe41af3517fb9103c38475d4
Teresa Zhou <111923005+TeresaZhou0127@users.noreply.github.com>
2024-11-14 17:30:22 -0600

    pom change

--- backend/src/main/java/com/backend/owlfinance/User/UserGlobalExceptionHandler.java
+++ backend/src/main/java/com/backend/owlfinance/User/UserGlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class UserGlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
