872baf625ff4b357ad5ed8be8247d2e850328c02
yunalee0206 <yuna2480@gmail.com>
2024-12-08 03:51:45 -0600

    deleted old directories/files

--- src/main/java/com/backend/owlfinance/repository/TradeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.owlfinance.repository;
-
-import com.backend.owlfinance.entity.TradeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TradeRepository extends JpaRepository<TradeEntity, String> {
-}
